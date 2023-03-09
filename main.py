90d7ee1cbb9787ef6ec6490717afd5344b67ecc4
rrex971 <75212090+rrex971@users.noreply.github.com>
2023-03-10 00:21:20 +0530

    Add nppp and current commands, update requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 from twitchio.ext import commands
+import websockets
+import asyncio
+false = False
+true = True
+null = None
+
 mods = "HDDTHRFLEZDTHDHRDTHRNM"
 config = open("config.txt")
 configlist = config.readlines()
@@ -34,7 +40,26 @@ async def r(ctx, link = "n", mods1 = "n"):
 async def helprequest(ctx):
 	await ctx.channel.send("Use the !r command to request maps, usage: !r <map link> [mods if any, leave blank for nm]")
 
+@bot.command(name = "np")
+async def np(ctx):
+    async with websockets.connect("ws://localhost:24050/ws") as ws:
+        data = eval(await ws.recv())["menu"]
+        print(data["bm"]["id"])
+        await ctx.send(f"""Now Playing: {data["bm"]["metadata"]["title"]} - {data["bm"]["metadata"]["artist"]} [{data["bm"]["metadata"]["difficulty"]}], ({data["bm"]["metadata"]["mapper"]} {data["bm"]["stats"]["SR"]}*) +{data["mods"]["str"]} | Link: https://osu.ppy.sh/b/{data["bm"]["id"]} """)
 
+@bot.command(name = "current")
+async def current(ctx):
+    async with websockets.connect("ws://localhost:24050/ws") as ws:
+        data = eval(await ws.recv())
+        dg = data["gameplay"]
+        await ctx.send(f"""Current: {dg["pp"]["current"]}pp / {dg["pp"]["fc"]}pp""")
+
+@bot.command(name = "nppp")
+async def nppp(ctx):
+    async with websockets.connect("ws://localhost:24050/ws") as ws:
+        data = eval(await ws.recv())
+        dm = data["menu"]
+        await ctx.send(f"""100% : {dm["pp"]["100"]}pp | 99% : {dm["pp"]["99"]}pp | 98% : {dm["pp"]["98"]}pp | 97% : {dm["pp"]["97"]}pp | 96% : {dm["pp"]["96"]}pp | 95% : {dm["pp"]["95"]}pp""")
 
 if __name__=="__main__":
     bot.run()
